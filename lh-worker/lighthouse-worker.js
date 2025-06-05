6a1da0bbefc2d66a582873a946bfbe79b20c8da7
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2025-06-06 01:18:00 +0300

    lh-worker supabase

--- lh-worker/lighthouse-worker.js
+++ lh-worker/lighthouse-worker.js
@@ -4,19 +4,20 @@ import * as chromeLauncher from 'chrome-launcher';
 import { S3Uploader } from './services/s3.js';
 import { getConfig as getLighthouseConfig } from './config/lighthouse.js';
 import { getConfig as getS3Config } from './config/s3.js';
+import { supabase } from './services/supa.js';
 
 class LighthouseWorker {
   constructor() {
     const lighthouseConfig = getLighthouseConfig();
     const s3Config = getS3Config();
-    
+
     this.urlToTest = lighthouseConfig.url;
     this.s3Uploader = new S3Uploader(s3Config);
   }
 
   async run() {
     const chrome = await this.launchChrome();
-    
+
     try {
       const runnerResult = await this.runLighthouseAudit(chrome);
       await this.processAndUploadResults(runnerResult);
@@ -55,6 +56,36 @@ class LighthouseWorker {
 
     await this.s3Uploader.upload(htmlKey, htmlResult, 'text/html');
     await this.s3Uploader.upload(jsonKey, jsonResult, 'application/json');
+
+    // Update the database with the results
+    const { data, error } = await supabase.from('lighthouse').update({
+      status: 'COMPLETED',
+    }).eq('id', process.env.TRACE_ID).select();
+
+    if (error) {
+      console.error('Error updating Lighthouse report status:', error);
+      throw new Error('Failed to update Lighthouse report status');
+    }
+
+    console.log('Lighthouse report updated: ', data);
+
+    const { insertData, error: insertError } = await supabase.from("lighthouse_results").insert([
+      {
+        relation_id: process.env.TRACE_ID,
+        html_report_url: `https://${getS3Config().bucketName}.s3.amazonaws.com/${htmlKey}`,
+        json_report_url: `https://${getS3Config().bucketName}.s3.amazonaws.com/${jsonKey}`,
+      }
+    ]).select();
+
+    console.log('insert result:', {
+      insertData,
+      error: insertError,
+    });
+
+    if (insertError) {
+      console.error('Error inserting Lighthouse report into Supabase:', error);
+      throw new Error('Failed to insert Lighthouse report into Supabase');
+    }
 
     console.log('Reports uploaded to S3:', { htmlKey, jsonKey });
   }
